5be831f151c7e610604df7bfce9405f918afb033
andres06-hub <andressierrarojas7@gmail.com>
2023-03-14 14:51:52 -0500

    ADD: properties of graph

--- backend/src/models/user.entity.ts
+++ backend/src/models/user.entity.ts
@@ -1,16 +1,27 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Review } from 'src/book/entities/review.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
+@ObjectType()
 @Entity({ name: 'user' })
 export class User {
+  @Field()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column('text')
   username: string;
 
+  @Field()
   @Column('text')
   email: string;
 
+  @Field()
   @Column('text')
   password: string;
+
+  @Field(() => [Review])
+  @OneToMany(() => Review, (review) => review.user)
+  reviews: Review[];
 }
