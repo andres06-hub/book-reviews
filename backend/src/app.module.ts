e78094673794ebc15fbab4f695127164ac11d120
andres06-hub <andressierrarojas7@gmail.com>
2023-03-14 14:51:06 -0500

    ADD: typeOrmModule sync and configuration GraphQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,30 @@ import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
 import { key } from './config/key.config';
 import { AuthModule } from './auth/auth.module';
-import { ProvidersModule } from './providers/providers.module';
+import { BookModule } from './book/book.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'node:path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseService } from './database/database.service';
 
 @Module({
-  imports: [AppModule, ConfigModule, AuthModule, ProvidersModule],
+  imports: [
+    AppModule,
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseService,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src', 'schema.gql'),
+    }),
+    AuthModule,
+    BookModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [DatabaseService],
   exports: [ConfigModule],
 })
 export class AppModule {
