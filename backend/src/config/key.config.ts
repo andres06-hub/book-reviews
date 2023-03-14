d32bcb2158a6af2ee6aa50f9afd78f7435ade448
andres06-hub <andressierrarojas7@gmail.com>
2023-03-14 14:49:32 -0500

    ADD: keys Db

--- backend/src/config/key.config.ts
+++ backend/src/config/key.config.ts
@@ -2,4 +2,9 @@ export enum key {
   PORT = 'PORT',
   PREFIX = 'PREFIX',
   JWT_KEY = 'JWT_KEY',
+  DB_HOST = 'DB_HOST',
+  DB_PORT = 'DB_PORT',
+  DB_USER = 'DB_USER',
+  DB_PWD = 'DB_PWD',
+  DB_NAME = 'DB_NAME',
 }
