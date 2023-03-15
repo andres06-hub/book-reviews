726dee32b2229b9acc692855604c359430d77bb1
andres06-hub <andressierrarojas7@gmail.com>
2023-03-15 17:34:51 -0500

    MODIFIED: change file position

---
+++ frontend/src/app/common/graphql.queries.ts
@@ -0,0 +1,54 @@
+import { gql } from 'apollo-angular';
+export const GET_BOOKS = gql`
+  query {
+    book {
+      id
+      title
+      linkImg
+      author
+      description
+    }
+  }
+`;
+
+export const CREATE_REVIEW = gql`
+  mutation createReview(
+    $comment: String!
+    $rating: Int!
+    $userId: Int!
+    $bookId: Int!
+  ) {
+    createReview(
+      createReview: {
+        comment: $comment
+        rating: $rating
+        userId: $userId
+        bookId: $bookId
+      }
+    ) {
+      id
+      comment
+      rating
+    }
+  }
+`;
+
+export const GET_DATA_USER = gql`
+  query user($userId: Int!) {
+    user(id: $userId) {
+      id
+      username
+      email
+    }
+  }
+`;
+
+export const GET_REVIEW_ONE_USER = gql`
+  query reviewOneUser($userId: Int!) {
+    reviewsOneUser(id: $userId) {
+      id
+      comment
+      rating
+    }
+  }
+`;
