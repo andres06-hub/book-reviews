5a637c91e4b40b58e9594a6ab67dcd1d167330a7
andres06-hub <andressierrarojas7@gmail.com>
2023-03-15 04:14:44 -0500

    CREATED: guard for path

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'not-found', pathMatch: 'full', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule) },
+  {
+    path: 'not-found',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./not-found/not-found.module').then((m) => m.NotFoundModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./signup/signup.module').then((m) => m.SignupModule),
+  },
+  {
+    path: 'home',
+    canActivate: [AuthGuard],
+    data: { redirectTo: '/login' },
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
   { path: '**', redirectTo: 'not-found' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
