304a5a55ddcefe6b5003615a0c635d599bf05b6b
andres06-hub <andressierrarojas7@gmail.com>
2023-03-14 21:53:41 -0500

    ADD: Service 'v1' is terminated to consume api

--- frontend/src/app/login/service/login.service.ts
+++ frontend/src/app/login/service/login.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { LoginInterface, ResponseAuth } from '../interfaces/login.interface';
 import { Router } from '@angular/router';
+import { CONSTANTS } from 'src/app/common/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private _http: HttpClient, private route: Router) {}
+  constructor(
+    private _http: HttpClient,
+    private route: Router,
+    ) {}
 
   login(data: LoginInterface) {
-    const headers = new HttpHeaders({ "Content-Type": "application/json" })
-    this._http.post<ResponseAuth>('http://localhost:3000/api/auth/login',
+    const headers: HttpHeaders = new HttpHeaders({ "Content-Type": "application/json" })
+    this._http.post<ResponseAuth>(`${CONSTANTS.AUTH_URL}/login`,
       data,
       {
         headers
@@ -22,13 +26,13 @@ export class LoginService {
           const { status, message, data } = response;
           console.log(status, message);
           if (!status) return; // Not login
-          if (data){
+          if (data) {
             window.sessionStorage.setItem('token', data.token);
             this.route.navigate(['/home'])
           }
         },
         error: (reject) => {
-          console.warn(reject);
+          console.error(reject);
         }
     });
   }
