924bd56845cbf829edfe52042fa5556f6c054cf1
andres06-hub <andressierrarojas7@gmail.com>
2023-03-15 22:52:24 -0500

    DELETED

--- frontend/src/app/common/constants.ts
+++
@@ -1,4 +0,0 @@
-export const CONSTANTS = {
-  AUTH_URL: 'http://localhost:3000/api/auth',
-  URL_GRAPHQL: 'http://localhost:3000/graphql',
-}
--- frontend/src/app/common/data.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Review {
-  id: string;
-  comment: string;
-  rating: number;
-  createAt: string;
-}
